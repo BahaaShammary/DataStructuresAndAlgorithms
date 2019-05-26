9eb743566f3b4c5d4aae613f7cf40abf316b2a9b
BahaaShammary <32821070+BahaaShammary@users.noreply.github.com>
2019-05-26 14:16:59 +0100

    Sorting Algorithms

--- Sorting Algorithms/Insertion Sort/InsertionSort.java
+++ Sorting Algorithms/Insertion Sort/InsertionSort.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+/**
+ * Sorting in this example means all items to the left of an item, are smaller than that item.
+ * This algorithm chooses a key (item from the list) and compares it to all previous items, if the key
+ * is smaller than an item before it, it swaps it and so on.
+ */
 public class InsertionSort {
     int list[];
     int size;
@@ -7,7 +12,6 @@ public class InsertionSort {
     public InsertionSort(int[] aList, int length) {
         list = aList;
         size = length;
-
     }
     public int[] start() {
         for (int i = 1; i < size;i++) { // start at 1 because the first item does not need to sort itself
--- Sorting Algorithms/Insertion Sort/README.md
+++ Sorting Algorithms/Insertion Sort/README.md
@@ -2,7 +2,11 @@
 
 Example use of Insertion Sort is to sort an array of items in increasing order. This array will contain items that are integers.
 [5, 8, 1, 3, 9, 6] 
+
 Sorting in this example means all items to the left of an item, are smaller than that item. 
+This algorithm chooses a key (item from the list) and compares it to all previous items, if the key is smaller than an item before it, it swaps it and so on.
 Insertion Sort is not a fast sorting algorithm because it uses nested loops to shift items into place. 
 It is only used for small data sets.
-It runs in O(n^2).
+
+Complexity: 
+It runs in O(n^2) – Because it runs using nested for loops.
